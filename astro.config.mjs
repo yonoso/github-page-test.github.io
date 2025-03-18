f1f0a2f3f4a94a9c9acb9f7e95ea18c36f721e60
yonoso <s.yonoue@inno-t.com>
2025-03-18 18:37:20 +0900

    修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yonoso.github.io/',
-  base: '/github-page-test/'
+  site: 'https://github-page-test.github.io/',
+  // base: '/github-page-test/'
 });
